20b48fdef8ba1af9fc9355bc0e99917ba95cd1f9
Your Name <you@example.com>
2023-01-11 21:26:01 +0700

    hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public name = 'Đỗ Thanh Linh'
+  public name = 'Đỗ Thanh Linh';
+  public age = 18;
   title = 'AngularTest';
 }
